7b31187183e2a7a4d5b3a46287be3b5e7288cbd7
Abhineet Raj <82033840+abhineetraj1@users.noreply.github.com>
2022-02-20 06:36:34 +0530

    Add files via upload

---
+++ README.md
@@ -0,0 +1,13 @@
+# Spaceship game
+Made fully in HTML, javascript, and css
+
+# Commands
+press "w" - to move ship up
+press "s" - to move ship down
+press "a" - to move ship left
+press "d" - to move ship right
+press "r" - to launch rocket
+
+Don't turn on the CapsLock, because it won't work
+
+Demo - [link](https://abhineetraj1.github.io/rocket-game/)
---
+++ game.js
@@ -0,0 +1,94 @@
+if(window.innerWidth<window.innerHeight){document.write("<center><h1>This game will not work in mobile device. Open this game in github.</h1></center>");}
+document.getElementById('ship').style.top="100px";
+document.getElementById('ship').style.left="100px";
+function ship_move(x,y) {
+	var a = document.getElementById('ship');
+	var x_s = parseInt(a.style.top.replace("px",""));
+	var y_s = parseInt(a.style.left.replace("px",""));
+	a.style.top= String(x_s+x)+"px";
+	a.style.left=String(y_s+y)+"px"
+}
+function ship_getPos() {
+	var x = parseInt(document.getElementById('ship').style.left.replace("px",""));
+	var y = parseInt(document.getElementById('ship').style.top.replace("px",""));
+	return {x,y};
+}
+function ship_set(x,y) {
+	document.getElementById('ship').style.top= String(x)+"px";
+	document.getElementById('ship').style.left=String(y)+"px"
+}
+var x1=ship_getPos().x;
+var y1=ship_getPos().y;
+window.onkeydown= function(e) {
+	console.log(e);
+	var spc=document.getElementById('ship').style;
+	if(e.key=="w") {
+		spc.top=y1 - 10+"px";
+		y1=y1-10;
+	} else if(e.key == "s") {
+		spc.top=y1 + 10+"px";
+		y1=y1+10;
+	} else if(e.key == "d") {
+		spc.left=x1 + 10+"px";
+		x1=x1+10;
+	} else if(e.key == "a") {
+		spc.left=x1 - 10+"px";
+		x1=x1-10;
+	} else if(e.key == "r") {
+		attack();
+	} else{
+		var n=0;
+	}
+}
+function sttt() {var p=document.createElement("p");p.innerHTML="Made By Abhineet Raj";p.style="text-align:center;height:5vh;padding:3px;font-size:3vh;background:white;color:black;position:absolute;top:90vh;float:right;";document.body.appendChild(p);}
+sttt();
+var g_over="<center><h1>Game Over!!</h1><br><h3>You have crossed the border line</h3></center>"
+function attack() {
+	var a = document.createElement("img");
+	a.src="rocket-launch.png";
+	a.className="roc";
+	a.style.position="absolute";
+	a.style.top=ship_getPos().y+"px";
+	a.style.left=ship_getPos().x+ document.getElementById('ship').width/2 +"px";
+	document.body.appendChild(a);
+}
+
+setInterval(check_over,1) 
+function check_over(){
+	var a = document.getElementById("ship");
+	if(ship_getPos().x==0){
+		document.write(g_over);
+		clearInterval(check_over,1);
+		clearInterval(rocket,100);
+	}else if(ship_getPos().y==0){
+		document.write(g_over);
+		clearInterval(check_over,1);
+		clearInterval(rocket,100);
+	}else if((ship_getPos().y+a.height) > window.innerHeight){
+		document.write(g_over);
+		clearInterval(check_over,1);
+		clearInterval(rocket,100);
+	}else if((ship_getPos().x+a.width)>window.innerWidth){
+		document.write(g_over);
+		clearInterval(check_over,1);
+		clearInterval(rocket,100);
+	} else{
+		var b=0;
+	}
+}
+setInterval(rocket,100);
+function rocket() {
+	var n=0;
+	while(n< document.getElementsByClassName('roc').length) {
+		document.getElementsByClassName('roc')[n].style.top= parseInt(document.getElementsByClassName('roc')[n].style.top.replace("px",""))-10 + "px";
+		if(parseInt(document.getElementsByClassName('roc')[n].style.top.replace("px",""))<0) {
+			document.getElementsByClassName('roc')[n].remove();
+		} 
+		n=n+1;
+	}
+
+}
+//  (x,y)       (x_a,y_a)
+//  (x_b,y_b)   (x_d,y_d)
+//
+//
